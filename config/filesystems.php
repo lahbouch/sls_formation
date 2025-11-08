e998ef4ce2b1a36c29c237b2ebb35e576439b77d
elviso <lahbouch.dev@gmail.com>
2025-11-09 00:27:18 +0100

    Update environment configuration to remove port from APP_URL and adjust related references in middleware and configuration files. Replace static image URLs in the navbar with dynamic asset paths for improved flexibility.

--- config/filesystems.php
+++ config/filesystems.php
@@ -39,7 +39,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => (env('APP_URL') ?: 'http://localhost:8000').'/storage',
+            'url' => (env('APP_URL') ?: 'http://localhost').'/storage',
             'visibility' => 'public',
             'throw' => false,
         ],
